a912f2d20b08c2c0e5f744aff565604e89ac43df
Honour-droid <60543508+Honour-droid@users.noreply.github.com>
2021-05-26 13:57:57 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+import os
+file = "Nicecock.mp3"
+image = "face.png"
+
+
+ass = input ('What size is dat ass? ')
+
+if ass == 'thicc'or ass == 'chonky':
+    print ("Nice Cock!")
+    os.system("mpg123 " + file)
+
+elif ass == 'big' or ass == 'huge':
+    print("awooga Mummy")
+
+elif ass == 'bubble butt' :
+    print("sit on my face UwU")
+    os.system("feh " + image)
+
+elif ass == 'small' or ass == 'pancake' :
+    print ("aww Baby need a bottle?")
+
+else: 
+    print("bit disappointing, you jealous?")
